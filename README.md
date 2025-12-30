a8274849e76a37c93e21f307a8d87e92c18b6ab8
Ashley <ashley.hughes@qcu87z.net>
2025-12-30 18:00:28 +1100

    more

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 ## Improved script
 
-```shell
+```bash
+python3 c.py
+Usage: c.py [-h] --prefix PREFIX --distance DISTANCE [--x X] [--y Y] filename
+
 python3 c.py <input csv> --prefix test1
+
+python3 c.py <input csv> --distance 900 --prefix test2
+
+python3 c.py <input csv> --distance 900 --prefix test3 --x 250 --y 250
 ```
 
 Where `input csv` is a section of a ShotMarker shoot.
@@ -12,7 +19,49 @@ Ensure a directory called `output` exists.
 
 It will then generate images with dynamic sizes but a min of -300,300 but will expand if required for a stray shot
 
-![example1](output/test1_m.png)
-![example2](output/test1_l.png)
+![example1](output/test_git_l.png)
+![example2](output/test_git_m.png)
 
 Current it is set for 900m Australian F-Class targets
+
+### Example input csv
+
+```csv
+time,tags,id,score,temp C,x mm,y mm
+11:08.5,sighter/incomplete,RS1,4,21,231,-249
+11:09.1,sighter/incomplete,MS1,6,21,43,-71
+11:09.4,off/sighter/warning-measured off target top/incomplete,LS1,0,21,729,1163
+11:11.2,sighter,RS2,5,20,-148,63
+11:11.5,sighter/incomplete,MS2,5,20,-3,-162
+11:12.3,sighter/incomplete,LS2,3,20,-333,358
+11:13.3,incomplete,R1,6,20,-64,-65
+11:14.1,,M1,5,20,-145,62
+11:15.2,incomplete,L1,4,20,367,111
+11:15.5,incomplete,R2,6,20,76,35
+11:16.1,incomplete,M2,5,20,-138,117
+11:16.3,incomplete,L2,5,20,37,137
+11:17.3,incomplete,R3,6,20,42,56
+11:18.4,,M3,6,21,65,-106
+11:20.1,incomplete,L3,4,21,403,-22
+11:21.4,,R4,X,21,43,-46
+11:22.2,incomplete,M4,4,21,80,-309
+11:23.0,,L4,5,21,227,37
+11:24.2,,R5,X,21,15,28
+11:24.6,,M5,6,21,83,-43
+11:25.4,incomplete,L5,4,21,-57,-342
+11:26.4,incomplete,R6,6,21,48,-108
+11:27.3,incomplete,M6,5,21,-146,-78
+11:28.5,incomplete,L6,X,21,16,17
+11:29.5,incomplete,R7,5,21,58,-121
+11:30.1,incomplete,M7,X,21,21,-11
+11:30.5,,L7,6,21,-126,16
+11:31.6,incomplete,R8,5,21,144,49
+11:32.3,incomplete,M8,5,21,-131,-191
+11:33.3,,L8,5,21,-7,168
+11:34.5,,R9,5,20,-13,180
+11:35.2,incomplete,M9,6,20,-94,-67
+11:35.6,incomplete,L9,6,20,130,-12
+11:36.5,incomplete,R10,6,20,9,124
+11:37.3,,M10,X,20,0,-42
+11:38.2,incomplete,L10,4,20,-276,-52
+```
--- c.py
+++ c.py
@@ -1,34 +1,35 @@
+from datetime import date
 import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 from matplotlib.patches import Circle
+import targets
 
-def save_plot(df, name):
+
+def save_plot(df, name, target, moa, distance, x, y):
     # Check if the DataFrame is empty aka no left shooter
     if df.empty:
         return
     _, ax = plt.subplots(figsize=(10, 10))
     ax.set_aspect("equal")
-
-    x_max_value = 300 if not df["x mm"].max() > 300 else df["x mm"].max()
-    x_min_value = -300 if not df["x mm"].min() < -300 else df["x mm"].min()
-    y_max_value = 300 if not df["y mm"].max() > 300 else df["y mm"].max()
-    y_min_value = -300 if not df["y mm"].min() < -300 else df["y mm"].min()
+    if x == 0:
+        x_max_value = 300 if not df["x mm"].max() > 300 else df["x mm"].max()
+        x_min_value = -300 if not df["x mm"].min() < -300 else df["x mm"].min()
+    else:
+        x_max_value = 300
+        x_min_value = -300
+    if y == 0:
+        y_max_value = 300 if not df["y mm"].max() > 300 else df["y mm"].max()
+        y_min_value = -300 if not df["y mm"].min() < -300 else df["y mm"].min()
+    else:
+        y_max_value = 300
+        y_min_value = -300
 
     ax.set_xlim(x_min_value - 50, x_max_value + 50)
     ax.set_ylim(y_min_value - 50, y_max_value + 50)
 
-    # 900m
-    rings = {
-        "(X)": 127 / 2,
-        "(6)": 255 / 2,
-        "(5)": 510 / 2,
-        "(4)": 815 / 2,
-        "(3)": 1120 / 2,
-        "(2)": 1830 / 2,
-    }
-    for label, radius in rings.items():
+    for label, radius in target.items():
         ring = Circle((0, 0), radius, color="black", fill=False, linewidth=1)
         ax.add_patch(ring)
         ax.annotate(
@@ -41,23 +42,21 @@ def save_plot(df, name):
             fontsize=7,
         )
 
-    major_spacing_xy = 261.8  # Major grid lines every 2 units on the x-axis
-    minor_spacing_xy = 130.9  # Minor grid lines every 0.5 units on the x-axis
+    major_spacing_xy = moa  # Major grid lines every 2 units on the x-axis
+    minor_spacing_xy = moa * 0.5  # Minor grid lines every 0.5 units on the x-axis
     ax.xaxis.set_major_locator(MultipleLocator(major_spacing_xy))
     ax.xaxis.set_minor_locator(MultipleLocator(minor_spacing_xy))
     ax.yaxis.set_major_locator(MultipleLocator(major_spacing_xy))
     ax.yaxis.set_minor_locator(MultipleLocator(minor_spacing_xy))
-    ax.grid(
-        which="major", linestyle="-", linewidth="1.5", alpha=0.5, color="gray"
-    )  # Customize major grid
-    ax.grid(
-        which="minor", linestyle="-", linewidth="1.0", alpha=0.5, color="gray"
-    )  # Customize minor grid
-    ax.set_title("900m - " + name + "\n\n29-Dec-2025")
+    ax.grid(which="major", linestyle="--", linewidth="0.75", alpha=0.5, color="gray")
+    ax.grid(which="minor", linestyle="-.", linewidth="0.75", alpha=0.5, color="gray")
+    today = date.today().strftime("%d-%b-%Y")
+    ax.set_title(f"{distance}m - {name}\n\n{today}")
 
     for _, row in df.iterrows():
         x, y = row["x mm"], row["y mm"]
-        label = row["id"]
+        s = row["id"].replace("L", "").replace("M", "").replace("R", "")
+        label = s + "\n(" + row["score"] + ")"
         if "S" in label:
             colour = "red"
         else:
@@ -74,17 +73,68 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="The csv file to process")
     parser.add_argument("--prefix", required=True, help="Prefix for the output files")
+    parser.add_argument(
+        "--distance", type=int, required=True, help="Distance in meters"
+    )
+    parser.add_argument("--x", type=int, help="X offset in mm", default=0)
+    parser.add_argument("--y", type=int, help="Y offset in mm", default=0)
     args = parser.parse_args()
 
-    df = pd.read_csv(args.filename)
+    match args.distance:
+        case 300:
+            target = targets.target_300m
+            moa = targets.moa_300m
+        case 500:
+            target = targets.target_500m
+            moa = targets.moa_500m
+        case 600:
+            target = targets.target_600m
+            moa = targets.moa_600m
+        case 700:
+            target = targets.target_700m
+            moa = targets.moa_700m
+        case 800:
+            target = targets.target_800m
+            moa = targets.moa_800m
+        case 900:
+            target = targets.target_900m
+            moa = targets.mmoa_900m
+        case _:
+            print("Unsupported distance:", args.distance)
+            return
 
+    df = pd.read_csv(args.filename)
     left_df = df[df["id"].str.startswith("L")]
     middle_df = df[df["id"].str.startswith("M")]
     right_df = df[df["id"].str.startswith("R")]
 
-    save_plot(left_df, "output/" + args.prefix + "_l.png")
-    save_plot(middle_df, "output/" + args.prefix + "_m.png")
-    save_plot(right_df, "output/" + args.prefix + "_r.png")
+    save_plot(
+        left_df,
+        "output/" + args.prefix + "_l.png",
+        target,
+        moa,
+        args.distance,
+        args.x,
+        args.y,
+    )
+    save_plot(
+        middle_df,
+        "output/" + args.prefix + "_m.png",
+        target,
+        moa,
+        args.distance,
+        args.x,
+        args.y,
+    )
+    save_plot(
+        right_df,
+        "output/" + args.prefix + "_r.png",
+        target,
+        moa,
+        args.distance,
+        args.x,
+        args.y,
+    )
 
 
 if __name__ == "__main__":
